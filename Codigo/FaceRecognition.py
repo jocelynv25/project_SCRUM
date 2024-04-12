e8f9f0361754873887ff401fac505660e05a20a2
Jocelyn V <jocelynvaz25@gmail.com>
2024-04-11 19:52:20 -0600

    Img prueba, leer y codificar imagenes

---
+++ Codigo/FaceRecognition.py
@@ -0,0 +1,9 @@
+import cv2 
+import numpy as np
+import face_recognition as fr
+import os 
+import random
+from datetime import datetime
+
+#Acceder a la carpeta en donde se tienen guardadas las imagenes
+path = 'imagenes_prueba'
