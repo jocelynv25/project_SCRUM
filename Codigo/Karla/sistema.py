a41b60ba8e62000e5dee320e747cdc1162102d7d
karlaelena2 <167023497+karlaelena2@users.noreply.github.com>
2024-04-14 12:33:41 -0600

    Add files via upload

---
+++ Codigo/Karla/sistema.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+def abrir_ventana2():
+    ventana1.withdraw()  
+    
+    ventana2 = tk.Toplevel()
+    ventana2.title("Sistema de Reconocimiento Facial")
+    ventana2.geometry("400x200")
+    
+    etiqueta = tk.Label(ventana2, text="¡Has entrado al sistema!", font=("Arial", 20))
+    etiqueta.pack(pady=50)
+
+
+ventana1 = tk.Tk()
+ventana1.title("Bienvenido al Sistema de Reconocimiento Facial")
+ventana1.geometry("400x400")
+ventana1.configure(bg='#FFFFFF')  
+
+marco_principal = tk.Frame(ventana1, bg='blue', padx=10, pady=10)  #  margen azul
+marco_principal.pack(expand=True, fill="both")
+
+imagen = Image.open("c:\\Users\\Dulce Gabriela\\Documents\\proyecto\\imagen.png")
+imagen_tk = ImageTk.PhotoImage(imagen)
+
+imagen_label = tk.Label(ventana1, image=imagen_tk, bg='#FFFFFF')
+imagen_label.pack(pady=5) 
+
+frame_contenedor = tk.Frame(ventana1, bg='#FFFFFF')
+frame_contenedor.pack(expand=True)
+
+cuadro_texto = tk.Label(frame_contenedor, text="SISTEMA DE RECONOCIMIENTO FACIAL", font=("Arial", 14), bg='#FFFFFF')
+cuadro_texto.pack(pady=5) 
+
+boton_entrar = tk.Button(frame_contenedor, text="ENTRAR AL SISTEMA", bg='#000099', fg="white", font=("Arial", 12), command=abrir_ventana2)
+boton_entrar.pack(pady=5, padx=50, ipadx=10, ipady=3)  
+
+ventana1.mainloop()
+ 
